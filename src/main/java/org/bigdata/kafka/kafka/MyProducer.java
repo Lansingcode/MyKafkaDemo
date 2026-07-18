93b1ff14ac0f1ada08bc5103c2c1cab16bd66e66
xujia <jxudut@outlook.com>
2026-07-18 21:20:21 +0800

    kafka生产者

--- src/main/java/org/bigdata/kafka/kafka/MyProducer.java
+++ src/main/java/org/bigdata/kafka/kafka/MyProducer.java
@@ -17,13 +17,18 @@ public class MyProducer {
     private final static String TOPIC_NAME="test1";
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        // 设置参数
         Properties props=new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"hadoop01.hdp.com:6667");
 
+        // 把发送的key从字符串序列化为字节数组
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
 
+        // 把发送的value从字符串序列号为字节数组
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
 
+        // 创建生产消息的客户端，传入参数
         Producer<String,String> producer=new KafkaProducer<String,String>(props);
 
         int msgNum=5;
@@ -31,9 +36,12 @@ public class MyProducer {
 
         for (int i=1;i<=10;i++){
             Order order=new Order(Long.valueOf(i),i);
+
+            // key：决定了往哪个分区上发，value：具体要发的消息内容
             ProducerRecord<String,String> producerRecord=new ProducerRecord<>(TOPIC_NAME,
                     order.getOrderId().toString(),JSON.toJSONString(order));
 
+            // 发送消息，得到元数据
             RecordMetadata recordMetadata=producer.send(producerRecord).get();
             System.out.println("异步方式发送消息结果： "+"topic-"+recordMetadata.topic()+"|partition-"+recordMetadata.partition()+"|offset-"+recordMetadata.offset());
         }
