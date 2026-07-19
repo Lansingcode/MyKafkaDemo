0f95f7ca9378eb7f5ca8bc56bfea9b9b9b33465b
xujia <jxudut@outlook.com>
2026-07-19 16:19:20 +0800

    kafka消费者

--- src/main/java/org/bigdata/kafka/kafka/MyConsumer.java
+++ src/main/java/org/bigdata/kafka/kafka/MyConsumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
@@ -12,21 +13,20 @@ import java.util.Properties;
 
 public class MyConsumer {
     private final static String TOPIC_NAME="test1";
-//    private final static String CONSUMER_GROUP_NAME="";
+    private final static String CONSUMER_GROUP_NAME="testGroup";
 
     public static void main(String[] args) {
         Properties props=new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"hadoop01.hdp.com:6667");
 
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG,CONSUMER_GROUP_NAME);
+        // 配置消费组
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,CONSUMER_GROUP_NAME);
 
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
-
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
 
         // 创建消费者客户端
-        KafkaConsumer<String,String> consumer= new KafkaConsumer<>(props);
+        KafkaConsumer<String,String> consumer= new KafkaConsumer<String,String>(props);
 
         // 订阅主题列表
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
